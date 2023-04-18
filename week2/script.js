1606255af6015fc2795ccb93771a0fba750de7c3
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-04-18 03:12:19 -0700

    Add files via upload

---
+++ week2/script.js
@@ -0,0 +1,8 @@
+let str1 = 'Laurence'; // type of string 
+let str2 = "Svekis"; //type of string 
+let val1 = undefined; // type of undefined
+let val2 = null; // type of null
+let myNum = 1000; // type of number
+console.log(str1, str2);
+console.log(val1, val2);
+console.log(myNum);
